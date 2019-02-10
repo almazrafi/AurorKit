6cc1490bfe96fbeb045235668f55f421ade313c5
Almaz Ibragimov <almazrafi@yandex-team.ru>
2019-02-10 16:13:51 +0300

    Codecov Fix - Fixed Travis CI configuration file (`.travis.yml`) - All configuration files have been added to the Xcode project (`.codecov.yml`, `.gitignore`, `.swiftlint.yml`, `.travis.yml`)

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -9,11 +9,11 @@ wget --output-document=$SWIFTLINT_PKG_PATH $SWIFTLINT_PKG_URL
 
 if [ -f $SWIFTLINT_PKG_PATH ]; then
   echo "SwiftLint package exists! Installing it..."
-  
+
   sudo installer -pkg $SWIFTLINT_PKG_PATH -target /
 else
   echo "SwiftLint package doesn't exist. Compiling from source..." &&
-  
+
   git clone https://github.com/realm/SwiftLint.git /tmp/SwiftLint &&
   cd /tmp/SwiftLint &&
   git submodule update --init --recursive &&
